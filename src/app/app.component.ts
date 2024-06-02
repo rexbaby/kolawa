fdefcf6ba044c13ee7b9770de4ce910096200f12
qin <qin@pollex.com.tw>
2024-06-03 03:16:31 +0800

    [update] 寫入一個沒有意義的文字假裝已經登入

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { RouterModule } from '@angular/router';
-import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterModule],
+  imports: [RouterModule, MatSnackBarModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'UmmTimeCard';
+
+  router = inject(Router);
+  snacker = inject(MatSnackBar);
+
+  constructor() {
+    sessionStorage.setItem('loginUser', '寫入一個沒有意義的文字假裝已經登入');
+    console.log(sessionStorage.getItem('loginUser'));
+    this.router.navigate(['/home']);
+    this.snacker.open(
+      '歡迎使用 UmmTimeCard, 已經寫入一個沒有意義的文字假裝已經登入',
+      '關閉',
+      { duration: 1000 },
+    );
+  }
 }
--- src/app/components/auth.guard.ts
+++ src/app/components/auth.guard.ts
@@ -3,7 +3,6 @@ import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
   if (sessionStorage.getItem('loginUser')) return true;
-
   const router = inject(Router);
 
   return router.navigate(['login']);
